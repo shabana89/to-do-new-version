a498490235dd9f76696e86b610948ebf91599596
shabana kauser <shabanakauser89@gmail.com>
2022-11-23 15:26:33 +0000

    Added a Form component with a basic function and added that component to the app.js page

---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+const Form = () => {
+  return (
+    <div>
+      <form>
+        <input placeholder="Add Todo Item"></input>
+        <button>Add</button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
